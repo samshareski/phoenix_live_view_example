2775b8758e94a9dac699980a56464c8458f9a4b6
Sam Shareski <sam_shareski@otpp.com>
2019-07-29 22:17:32 -0400

    Update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Demo.Mixfile do
     [
       app: :demo,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -47,7 +47,7 @@ defmodule Demo.Mixfile do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:cowboy, "~> 2.0"},
-      {:calendar, "~> 0.17.5"},
+      {:calendar, "~> 0.17.5"}
     ]
   end
 
